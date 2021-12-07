40d8a8c11be451b7a15918d47935cb664cef135a
Tim Washington <taw@tehmwashington.com>
2021-12-07 15:48:17 -0600

    12-07 EOD push

--- Week_01/javascript-project-solar-calculator/2-javascript-project-solar-calculator-m2-exercise-files/startJS/js/main.js
+++ Week_01/javascript-project-solar-calculator/2-javascript-project-solar-calculator-m2-exercise-files/startJS/js/main.js
@@ -1,1 +1,26 @@
 // JavaScript Document
+function floor(diameter) {
+
+    var radius = diameter/2;
+    var floorArea = Math.PI * (radius * radius);
+    return floorArea;
+}
+
+function walls(diameter, height) {
+    var radius = diameter/2;
+    var circumference = 2 * Math.PI * radius;
+    var wallArea = circumference * height;
+    return wallArea;
+}
+
+function materialsNeeded() {
+    var d = document.getElementById('across').value;
+    var h = document.getElementById('height').value;
+
+    var carpetNeeded = Math.ceil(floor(d));
+    var paintNeeded = Math.ceil(walls(d, h));
+
+    console.log(`Carpet Needed is ${carpetNeeded} sqft`);
+    console.log(`Paint Needed is ${paintNeeded} sqft`);
+}
+
--- Week_01/javascript-project-solar-calculator/3-javascript-project-solar-calculator-m3-exercise-files/practice/js/main.js
+++ Week_01/javascript-project-solar-calculator/3-javascript-project-solar-calculator-m3-exercise-files/practice/js/main.js
@@ -1,7 +1,7 @@
 
 function showStuff(el) {
 var elementID = document.getElementById(el);
-	for (i=0; i<elementID.length; i++) {
+	for (i = 0; i < elementID.length; i++) {
 	if (elementID[i].selected=== true) {
 		var x = elementID[i].text;
 	} // end if
@@ -9,12 +9,11 @@ var elementID = document.getElementById(el);
 	return x;
 } // end function
 
-
 function showMore(el) {
 	var elementID = document.getElementsByName(el);
 	//console.log(elementID.length);
 	var mycheck = "";
-	for (i=0; i<elementID.length; i++) {
+	for (i = 0; i < elementID.length; i++) {
 			//console.log(elementID[i].checked);
 			//console.log(elementID[i].value);
 			if (elementID[i].checked === true) {
@@ -24,19 +23,12 @@ function showMore(el) {
 	} // end of loop
 
 return mycheck;
-	
-
 } // end of function
-
 
 function evaluatePage() {
 	var feedbackState = showStuff("state");
 	var feedbackHome = showStuff("home");
 	var feedbackLight = showMore('bulb');
 	document.getElementById("output").innerHTML = feedbackState +'<br>' +feedbackHome +'<br>'+ feedbackLight;
-	
-
-
-	
 }
 
